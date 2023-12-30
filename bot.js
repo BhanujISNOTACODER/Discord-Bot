8513992c1e9588726ce85e08bceb9e8fa55666ca
Bhanuj Sharma <bhanujsharma8990@gmail.com>
2023-12-30 11:43:09 +0530

    Added kick functionality

--- bot.js
+++ bot.js
@@ -11,10 +11,23 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent,
     IntentsBitField.Flags.GuildMessageReactions,
     IntentsBitField.Flags.GuildMessageTyping,
+    IntentsBitField.Flags.GuildScheduledEvents,
     ]
  });
 
+const kickMember = async(memberToKick,message) =>{
+    try {
+        message.reply(`Successfully removed ${memberToKick.user.tag}.`);
+        await memberToKick.kick();
+      } catch (error) {
+        console.error(error);
+        message.reply('Error kicking the member.');
+      }
+}
+
 const prefix = '$'
+
+
 
 client.on('ready',()=>{console.log("I am ready")});
 
@@ -22,9 +35,31 @@ client.on('messageCreate',(message)=>{
     // check if bot wrote the message
     if(message.author.bot) return;
 
-    // hello 
+    // hello command
     if(message.content==="Hello Principal"){
         message.reply("Hello,beta")
     }  
-})
+
+    if(message.content.startsWith(prefix)){
+        const [cmd,...args] = message.content.substring(prefix.length).trim().split(/\s+/)
+        if(cmd==="kick"){
+            if(!message.member.permissions.has("KICK_MEMBERS")){
+                message.reply("You do not have permissions")
+            }
+            else if(args.length===0){
+                message.reply("Please provide appropriate username")
+            }
+            else{
+                const memberToKick = message.mentions.members.first();
+                if(!memberToKick){
+                    message.reply("No such member exists in the server")
+                    return;
+                } 
+                 kickMember(memberToKick,message);
+                }
+            }
+        }
+        
+    }
+)
 client.login(process.env.DISC_BOT_TOKEN)
